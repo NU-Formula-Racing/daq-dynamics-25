ab85cfc88ee22fb107fdf8a460f2cefb0d4634db
cjhandchy <zhouxuyi@shphshool.com>
2025-04-08 20:17:59 -0500

    Changes to CAN

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 
 int currentTime;
 int cloopTime; 
+float pot_resis = 1000;
+//Just a Temporary Value, Should be changed based on the actual resistor used in hardware
 
 //Air Speed Object Setup
 HWPin air_speed_pins[] = {AIR_SPEED_0, AIR_SPEED_1, AIR_SPEED_2, AIR_SPEED_3, AIR_SPEED_4, AIR_SPEED_5, AIR_SPEED_6, AIR_SPEED_7};
@@ -18,13 +20,14 @@ AirSpeed* air_speed_objects[7];
 float air_speed_values[7];
 
 // Flow Rate Objects
-FlowRate flow_rate_object = FlowRate(FLOW_0);
-FlowRate flow_rate_object = FlowRate(FLOW_1);
+FlowRate flow_rate_object_mot = FlowRate(FLOW_0);
+FlowRate flow_rate_object_acc = FlowRate(FLOW_1);
 float flow_rate_values[1];
 //To be Implemented in the Loop
 
 // Thermistor Objects
-Thermistor thermistor_object = Thermistor(34, 35); 
+Thermistor thermistor_object_mot = Thermistor(TEMP_0, pot_resis); 
+Thermistor thermistor_object_acc = Thermistor(TEMP_1, pot_resis); 
 float coolant_temperature_values[1];
 //To be Implemented in the loop
 
@@ -59,10 +62,19 @@ void loop()
       air_speed_values[i] = air_speed_objects[i]->readAirspeed();
    }
 
-   // package the values into an array
-   // air_speed_values
-   // coolant_values ..
+      // Fills array with temp_values
+      coolant_temperature_values[0] = thermistor_object_mot.Read_Temp();
+      coolant_temperature_values[1] = thermistor_object_acc.Read_Temp();
+      // Fills array with flow_rate_values
+      flow_rate_values[0] = flow_rate_object_mot.getFlowRate();
+      flow_rate_values[1] = flow_rate_object_acc.getFlowRate();
+   //Accessing the IMU values need to be implemented
 
-   updateSignals(air_speed_values, coolant_temperature_values, flow_rate_values);
+   // package the values into arrays
+   std::array<float, 8> Air_Speed_Vals = {air_speed_values[0], air_speed_values[1], air_speed_values[2], air_speed_values[3], air_speed_values[4], air_speed_values[5], air_speed_values[6], air_speed_values[7]};
+   std::array<float, 2> Coolant_Temperature_Vals = {coolant_temperature_values[0], coolant_temperature_values[1]};
+   std::array<float, 2> Flow_Rate_Vals = {flow_rate_values[0], flow_rate_values[1]};
+   std::array<float, 6> IMU_Vals = {imu_values[0], imu_values[1], imu_values[2],imu_values[3],imu_values[4],imu_values[5]};
+   updateSignals(Air_Speed_Vals, Coolant_Temperature_Vals, Flow_Rate_Vals, IMU_Vals);
    can_bus.Tick();
 }
